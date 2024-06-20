d12b10ad39f27da812bb12afb76589fbfc459d73
karim <karim.itani@hec.ca>
2024-06-20 12:29:52 -0400

    adding attractions and accomodations pages

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+// navbar.js
+export const navbarTemplate = `
+<nav>
+    <ul class="navbar_items">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="accomodations.html">Accomodations</a></li>
+        <li><a href="attractions.html">Attractions</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+</nav>
+`;
